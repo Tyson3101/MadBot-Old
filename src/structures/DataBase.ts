5d68501bc64cb715f1df742d3bc4b1c6b40332d4
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2020-11-03 22:28:04 +1100

    Update DataBase.ts

--- src/structures/DataBase.ts
+++ src/structures/DataBase.ts
@@ -2,4 +2,4 @@ import Keyv from "keyv";
 import { config } from "dotenv";
 config();
 export const guildDataBase = new Keyv(process.env.MONGODBPassword); // DataBase Connetction
-guildDataBase.on("error", console.error);
+guildDataBase.on("error", console.error); // Handles Errors
